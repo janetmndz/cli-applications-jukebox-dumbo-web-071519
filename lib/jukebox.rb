f5dce0f071304b53a98de15b6132dce9bf953226
Janet Mendez <janetmendez.03@gmail.com>
2019-07-05 23:59:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,6 +1,6 @@
 def help
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
+  puts "I accept the following commands:
+  - help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
   puts "- exit this program"
