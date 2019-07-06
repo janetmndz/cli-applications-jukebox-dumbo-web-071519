e9d15a35c2e22ff66377e027d0fb56cc2bcbcf41
Janet Mendez <janetmendez.03@gmail.com>
2019-07-06 01:13:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,13 +7,15 @@ def help
 end
 
 def play(songs)
-  song_request = gets.chomp #this will be a title or a number
-  
-  if songs.include?(song_request)
+  song_request = gets.chomp ### this will be a title or a number
+  if songs.include?(song_request) ### check if this is a full song title
     p song_request
-  elsif song_request.to_i.to_s == song_request && songs[(song_request.to_i - 1)]
+  elsif song_request.to_i.to_s == song_request && songs[(song_request.to_i - 1)] ### check if this is a number && it's included in array
     p songs[(song_request.to_i - 1)]
   else
     p "Invalid input, please try again"
   end 
 end
+
+def list(songs)
+end 
