6c930b1e5065ca15faee591c02891779c5bd996f
Janet Mendez <janetmendez.03@gmail.com>
2019-07-06 00:24:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,8 +8,8 @@ end
 
 def play(songs)
   song_request = gets.chomp #this will be a title or a number
-  if songs[song_request.to_i + 1]
-    p songs[song_request.to_i + 1]
+  if songs[song_request.to_i]
+    p songs[song_request.to_i]
   elsif song.include? (song_request)
     p song_request
   else
